a67c12c2abd22e9df9cbba0446091dc988871266
willlandricombe <165650312+willlandricombe@users.noreply.github.com>
2024-04-01 12:27:21 +0100

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,13 @@
 print("My FizzBuzz Game")
 
 for i in range (1,101):
+  if i % 3 == 0:
+    output += Fizz
   
+  if i % 5 == 0:
+    output += Buzz
+  
+  if output == "":
+    output = i
+
+  print(output)
